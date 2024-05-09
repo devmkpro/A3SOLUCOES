13ed1aecf5b838896a7961919bacb0143d1998d6
_devmk <95938240+devmkpro@users.noreply.github.com>
2024-05-09 19:06:24 -0300

    Update task index table headers to use dark mode styles

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -65,23 +65,23 @@
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-200">
                         <tr>
-                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ __('Title') }}
                             </th>
 
-                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ __('Description') }}
                             </th>
 
-                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ __('Expires at') }}
                             </th>
 
-                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ __('Status') }}
                             </th>
 
-                            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                            <th scope="col" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
                                 {{ __('Actions') }}
                             </th>
 
