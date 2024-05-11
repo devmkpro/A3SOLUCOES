1e229849e56340b05a266265ffa5edd11205d0f2
maikeofc18 <maikeofc18@gmail.com>
2024-05-11 04:32:21 +0000

    profileupdate

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Rules\ValidateCPF;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Rules\ValidateBirthDate;
 
 class ProfileUpdateRequest extends FormRequest
 {
@@ -20,7 +21,7 @@ class ProfileUpdateRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore(auth()->user()->id)],
             'cpf' => ['required', 'string', 'max:14', Rule::unique(User::class)->ignore(auth()->user()->id), new ValidateCPF()],
-            'birth_date' => ['required', 'date', 'before:today'],
+            'birth_date' => ['required', 'date', 'before:today', new ValidateBirthDate()],
         ];
     }
 }
