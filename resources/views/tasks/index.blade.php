1b6d3b6fa087b8f1d37fba9dd3819edc81f98db3
maikeofc18 <maikeofc18@gmail.com>
2024-05-11 19:19:15 +0000

    js

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -303,7 +303,19 @@
             </div>
         </div>
 
-        <script src="{{ asset('Task/index.js') }}"></script>
+        <script>
+
+
+        document.querySelectorAll('.delete-btn').forEach(button => {
+        button.addEventListener('click', () => {
+            const taskId = button.dataset.taskId;
+            const modal = document.querySelector('#popup-modal');
+            const form = modal.querySelector('form');
+            form.action = form.action.replace('task_id', taskId);
+            form.querySelector('input[name="task_id"]').value = taskId;
+        });
+        });
+        </script>
     @endif
 
 
