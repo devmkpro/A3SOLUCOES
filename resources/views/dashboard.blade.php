d670ae145655f79c2cabcd928c546f8ba05cde7c
devmkpro <maikeofc18+1@gmail.com>
2024-06-10 20:09:27 -0300

    index reformulado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,19 +8,52 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-4">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h2 class="text-lg font-semibold mb-2">{{ __('Tasks') }}</h2>
-                    <p>{{ __('You have :count tasks pending', ['count' => auth()->user()->tasks()->where('status', '=', 'pending')->count()]) }}
-                    </p>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                @if (auth()->user()->is_admin)
+                    <div class="p-6 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg shadow">
+                        <div class="flex items center">
+                            <div class="ml-4">
+                                <h2 class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                                    {{ __('Users') }}
+                                </h2>
+                                <p class="text-sm text-gray-600 dark:text-gray-300">
+                                    {{ __('The system has :count users', ['count' => \App\Models\User::count()]) }}
+                                </p>
+                            </div>
 
-                    </p>
+                        </div>
+                        <div class="flex justify-end mt-4">
+                            <a type="button"
+                                href="{{ route('users.index') }}"class="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                {{ __('View Users') }}
+                            </a>
+                        </div>
+                    </div>
+                @endif
+
+                <div class="p-6 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg shadow">
+                    <div class="flex items center">
+
+                        <div class="ml-4">
+                            <h2 class="text-lg font-semibold text-gray-700 dark:text-gray-200">Tarefas Pendentes</h2>
+                            <p class="text-sm text-gray-600 dark:text-gray-300">
+                                {{ __('You have :count tasks pending', ['count' => \App\Models\Task::where('status', 'pending')->count()]) }}
+                            </p>
+                        </div>
+                    </div>
+
+
+                    <div class="flex justify-end mt-4">
+                        <a type="button"
+                            href="{{ route('tasks.index') }}"class="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            {{ __('View Tasks') }}
+                        </a>
+
+                    </div>
                 </div>
-
-                <a href="{{ route('tasks.index') }}"
-                    class="block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('View Tasks') }}</a>
-                    
             </div>
         </div>
     </div>
+
+
 </x-app-layout>
